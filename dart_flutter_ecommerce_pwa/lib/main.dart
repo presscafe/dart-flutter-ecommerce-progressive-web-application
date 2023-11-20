1867c17881bb270514f99618092b825d7ab8d18b
Farouk Pandor <presscafeworld@gmail.com>
2023-11-20 10:14:26 +0000

    commit

--- dart_flutter_ecommerce_pwa/lib/main.dart
+++ dart_flutter_ecommerce_pwa/lib/main.dart
@@ -1,125 +1,181 @@
+// lib/main.dart
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ecommerce PWA',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Ecommerce PWA'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Welcome to the Ecommerce PWA!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            // Recommended Ecommerce Widgets and Features
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality for browsing products
+                // Navigate to a responsive product listing page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductListingPage()));
+              },
+              child: Text('Browse Products'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality for viewing the shopping cart
+                // Navigate to a responsive shopping cart page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ShoppingCartPage()));
+              },
+              child: Text('View Cart'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality for user authentication or login
+                // Navigate to a secure and responsive login page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              child: Text('Login'),
+            ),
+            // Additional Features
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality for user registration
+                // Navigate to a user-friendly and secure registration page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => RegistrationPage()));
+              },
+              child: Text('Register'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality for viewing order history
+                // Navigate to a responsive order history page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => OrderHistoryPage()));
+              },
+              child: Text('Order History'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class Product {
+  final String name;
+  final String description;
+  final double price;
+
+  Product(this.name, this.description, this.price);
+}
+
+class ProductListingPage extends StatelessWidget {
+  final List<Product> products = [
+    Product('Smartphone', 'High-end smartphone with advanced features.', 799.99),
+    Product('Laptop', 'Powerful laptop for work and entertainment.', 1299.99),
+    Product('Wireless Headphones', 'Premium wireless headphones with noise cancellation.', 199.99),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Listing'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].name),
+            subtitle: Text(products[index].description),
+            trailing: Text('\$${products[index].price.toString()}'),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement the shopping cart page UI and functionality
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping Cart'),
+      ),
+      body: Center(
+        child: Text('Shopping Cart Page'),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement the login page UI and functionality
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
+
+class RegistrationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement the registration page UI and functionality
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registration'),
+      ),
+      body: Center(
+        child: Text('Registration Page'),
+      ),
+    );
+  }
+}
+
+class OrderHistoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Implement the order history page UI and functionality
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Order History'),
+      ),
+      body: Center(
+        child: Text('Order History Page'),
+      ),
+    );
+  }
+}
+
